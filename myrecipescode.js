b8f2107211fcf8fb758cf697b239ab868de66ef0
Emelie Johansen <emelie_johansen@hotmail.com>
2021-04-22 16:07:31 +0200

    Lagt till en ny fil om recept koder

---
+++ myrecipescode.js
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project: Recipe book</title>
+        <meta charset="utf-8">
+        <style>
+        body {
+            background-color: rgb(5, 213, 240);
+        }
+        h1 {
+            color: rgb(118, 10, 135);
+        }
+        h2 {
+            color: rgb(14, 5, 138);
+        }
+        
+        .how-to-do {
+            background-color: rgb(26, 14, 245);
+        }
+        </style>
+    </head>
+    <body>
+       
+        <h1>Emelie's Recipe Book</h1>
+        
+        <h2>Contents:</h2>
+        
+        <ol>
+            <li><a href="#pancakes">Pancakes</a></li>
+            <li><a href="#cinnamon-buns">Cinnamon Buns</a></li>
+            <li><a href="#chocolate-chip-cookies">Chocolate Chip Cookies</a></li>
+        </ol>
+        
+        <h2 id="pancakes">Pancakes</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Blueberry_pancakes_%283%29.jpg/330px-Blueberry_pancakes_%283%29.jpg" alt= "pancakes" width = "300">
+        
+        <ul>
+            <li>Time: 20 min</li>
+            <li>Serves: 20 pancakes</li>
+        </ul>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th>Ingredients</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Flour, butter, sugar and salt</td>
+                <td>20 pieces</td>
+            </tr>
+            </tbody>
+        </table>
+        
+        <p class="how-to-do"><strong>How to do it:</strong> Mix all the ingredients together and fry them on the stove. Serve them and enjoy!
+</p>
+        
+    <p><em>Source: <a target="_blank" href="https://mittkok.expressen.se/recept/veganska-pannkakor/">Pancakes recipe</a></em></p>
+    
+    <h2 id="cinnamon-buns">Cinnamon Buns</h2>
+    
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Kanelbulle.jpg/375px-Kanelbulle.jpg" alt="one cinnamon bun" width = "300">
+    
+            <ul>
+            <li>Time: 1 hour</li>
+            <li>Serves: 30 cinnamon buns</li>
+        </ul>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th>Ingredients</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Flour, butter, sugar, salt, yeast, water, vanilla sugar, cinnamon, brown sugar </td>
+                <td>30 pieces</td>
+            </tr>
+            </tbody>
+        </table>
+        
+        <p class="how-to-do"><strong>How to do it:</strong> Mix all the ingredients together except for the butter and flour. Put then in butter and flour and start working the dough with your hands. Roll out the dough on the table and fill it with the filling and then let them fermet for 1 hour and then put them in the oven for 8 minutes.
+</p>
+        
+    <p><em>Source: <a target="_blank" href="https://blogg.alltommat.se/fridasbakblogg/2018/08/19/veganska-kanelbullar/">Cinnamon buns recipe</a></em></p>
+    
+    <h2 id="chocolate-chip-cookies">Chocolate Chip Cookies</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2ChocolateChipCookies.jpg/330px-2ChocolateChipCookies.jpg" alt= "two chocolate chip cookies" width = "300">
+        
+        <ul>
+            <li>Time: 20 min</li>
+            <li>Serves: 20 cookies</li>
+        </ul>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th>Ingredients</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Flour, butter, sugar, salt, vanilla sugar, syrup, bicarbonate, corn starch and chocolate</td>
+                <td>20 pieces</td>
+            </tr>
+            </tbody>
+        </table>
+        
+        <p class="how-to-do"><strong>How to do it:</strong> Whish sugar and butter together and then mix in all the other ingredients. Chop the chocolate in small pieces and mix it in to the dough. Roll them into small pieces and put them on baking paper on a plate in the oven for 14 minutes. Let them cool for a while and then enjoy them!
+</p>
+        
+    <p><em>Source: <a target="_blank" href="https://www.ellenfarnstrom.se/2017/09/01/sega-chocolate-chip-cookies/">Chocolate Chip Cookies recipe</a></em></p>
+    
+    </body>
+</html>
