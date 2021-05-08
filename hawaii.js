5108da2cb96b89e2cda3ecb4b106a2c84619fb57
Emelie Johansen <emelie_johansen@hotmail.com>
2021-05-08 20:41:58 +0200

    Lagt till nya filer

---
+++ hawaii.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Project: Travel webpage</title>
+    </head>
+    <body>
+        <h1>Travel to Hawaii!</h1>
+        <style>
+            body {
+                background-color: rgb(3, 250, 201);
+            }
+            h2 {
+                color: rgb(28, 9, 153);
+            }
+           #places-to-visit {
+               background-color: rgb(41, 41, 230);
+           }
+           #food-fruits {
+               background-color: rgb(0, 180, 240);
+           }
+           .common-fruits {
+               background-color: rgb(58, 252, 10);
+           }
+        </style>
+    </body>
+    
+    <h2>Places to visit in Hawaii</h2>
+    <p id="places-to-visit"> Here are some places that <strong>you have to visit</strong> when you are traveling to Hawaii!
+    <li>Beautiful beaches</li>
+    <li>Beautiful mountains</li>
+    <li>All the restaurants with amazing food</li>
+    </p>
+    <img src = "https://cdn.kastatic.org/third_party/javascript-khansrc/live-editor/build/images/landscapes/beach-in-hawaii.png" alt="Beautiful blue ocean and a mountain in the background" width = "400" >
+    <img src ="https://cdn.kastatic.org/third_party/javascript-khansrc/live-editor/build/images/landscapes/mountains-in-hawaii.png" alt="Beautiful mountain in Hawaii" width = "400">
+    <p id = "food-fruits"> There is so many beautiful beaches and mountains here and there is also alot of <em>good food</em>.<br>
+    Fruit is very common here and everyone eats tons of fruits here every year!</p> 
+    <img src ="https://cdn.kastatic.org/third_party/javascript-khansrc/live-editor/build/images/food/fruits.png" alt="Different fruits like grapes, pineapple and peaches" width = "400">
+    <p class="common-fruits">Here are some common fruits that you can find in Hawaii</p>
+    <li>Mango</li>
+    <li>Papaya</li>
+    <li>Pineapple</li>
+    <li>Starfruit</li>
+    <li>Guava</li>
+</html>
